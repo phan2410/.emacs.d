ec899c9cb1fe71a301aba88bdb10a5c645f6c6a6
Phan Tom <phantom2410@live.com>
2018-07-17 06:16:00 +0700

    initial commit

---
+++ init.el
@@ -0,0 +1,27 @@
+;; Add Repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+         '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Load An's favorite theme
+(add-to-list 'load-path "~/.emacs.d/themes/elisp" t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes" t)
+(load-theme 'monokai t)
+
+
+;;Startup Config for MSWindows
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(show-paren-mode 1)
+
