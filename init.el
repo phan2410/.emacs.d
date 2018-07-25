a722454f759d2d17555ad6c54eeb74e3224052c5
fanPhone <phan2410@gmail.com>
2018-07-25 10:59:01 +0700

    add control seq 4 win

--- init.el
+++ init.el
@@ -16,12 +16,14 @@
 (load-theme 'monokai t)
 
 
-;;Startup Config for MSWindows
+;;Startup Config
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-message t)
+
+(if (string= system-type 'windows-nt)
+  (progn (tool-bar-mode -1)	 
+	 (scroll-bar-mode -1)))
 (menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
 
 (show-paren-mode 1)
 
